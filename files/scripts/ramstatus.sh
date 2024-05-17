9fd82b99a5f654f12fcfd5a2796f3a83fa1a3a7b
dkadev <23488064+dkadev@users.noreply.github.com>
2024-05-17 23:02:30 +0200

    New taskbar icons

--- files/scripts/ramstatus.sh
+++ files/scripts/ramstatus.sh
@@ -4,12 +4,12 @@ USED_MEM=$(free --giga | awk '/^Mem:/ {printf "%d", $3}')
 FREE_MEM=$(free -h --giga | awk '/^Mem:/ {print $3 "/" $2}')
 
 if [ ${USED_MEM} -gt 5 ]; then
-        echo "<txt>  <span foreground=\"red\">$FREE_MEM </span></txt>"
+        echo "<txt>  <span foreground=\"red\">$FREE_MEM </span></txt>"
         echo "<tool>RAM info</tool>"
 elif [ ${USED_MEM} -gt 2 ]; then
-        echo "<txt>  <span foreground=\"yellow\">$FREE_MEM </span></txt>"
+        echo "<txt>  <span foreground=\"yellow\">$FREE_MEM </span></txt>"
         echo "<tool>RAM info</tool>"
 else
-        echo "<txt>  $FREE_MEM </txt>"
+        echo "<txt>  $FREE_MEM </txt>"
         echo "<tool>RAM info</tool>"
 fi
